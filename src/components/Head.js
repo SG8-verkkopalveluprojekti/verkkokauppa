e1dcafa3599a5527c1f963dd9c77dc3ec52063ea
Aki <aki.stromberg@gmail.com>
2023-11-10 15:59:31 +0200

    search bar, and header adjustments

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,26 +1,25 @@
-
-import React from 'react'
+import React from "react";
 
 export const Head = () => {
   return (
     <div>
-        <section className='head'>
-            <div className='container d_flex'>
-                <div className='left row'>
-                    <i className='fa fa-phone'></i>
-                    <label>+358 44 000 000</label>
-                    <i className='fa fa-envelope'></i>
-                    <label>example@example.com</label>
-                </div>
-                <div className='right row RText'></div>
-                <label>Need Help?</label>
-                <label>Need Help?</label>
-                <span>🏴‍☠️</span>
-                <label htmlFor=''>FI</label>
-                <span>🏁</span>
-                <label htmlFor=''>EUR</label>
-            </div>
-        </section>
+      <section className="head">
+        <div className="container d_flex">
+          <div className="left-column">
+            <i className="fa fa-phone"></i>
+            <label>+358 44 000 000</label>
+            <i className="fa fa-envelope"></i>
+            <label>example@example.com</label>
+          </div>
+          <div className="right row RText"></div>
+          <label>Need Help?</label>
+          <label>Need Help?</label>
+          <span>🏴‍☠️</span>
+          <label htmlFor="">FI</label>
+          <span>🏁</span>
+          <label htmlFor="">EUR</label>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,28 @@
+
+
 import React from 'react'
+
 
 export const Search = () => {
   return (
     <>
-    <h1>Search</h1>
+    <section className='search'>
+      <div className='container c_flex'>
+        <div className='search-box f_flex'>
+          <i className='fa fa-search'></i>
+          <input type='text' placeholder='Etsi tuotteita' ></input>
+          <span>Kaikki tuotteet</span>
+        </div>
+        <div className='icon f_flex width'>
+          <div className='cart'>
+          {/* <Link to="/cart"> */}
+              <i className='fa fa-shopping-bag icon-circle'></i>
+              <span>0</span>
+            {/*  </Link> */}
+          </div>
+        </div>
+      </div>
+    </section>
     </>
   )
 }
