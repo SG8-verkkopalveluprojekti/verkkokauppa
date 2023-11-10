38e39bcc74d2381fd71446c66a335a06a7f8cd37
Aki <aki.stromberg@gmail.com>
2023-11-10 13:14:34 +0200

    fixed issue with navi

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       
-      <Navi />
+      
       <Header />
       tähän avataan verkkokauppa!
       <Cart />
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,14 @@ import './Header.css';
 import {  Head  } from "./Head";
 import React from 'react'
 import { Search } from './Search';
-import { Nav } from './Navi';
+import { Navi } from './Navi';
 
 export const Header = () => {
   return (
     <>
       <Head />
       <Search />
-      <Nav />
+      <Navi />
     </>
   )
 }
