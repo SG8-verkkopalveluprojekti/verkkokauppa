5af5067b93e04c07c4f1fa7b540c11ff3fd9d224
tyni99 <topias.tyni@hotmail.com>
2023-11-10 12:57:03 +0200

    navbar lisäys

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
-import { Nav } from "./components/Nav";
+import { Navi } from "./components/Nav";
 import { Cart } from "./components/Cart";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <Navi />
       <Header />
       tähän avataan verkkokauppa!
       <Cart />
