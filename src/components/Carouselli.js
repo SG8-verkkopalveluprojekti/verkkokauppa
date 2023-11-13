51dffa2a35072709ab07f25b4022b7777d021aa6
Jonssoni <n2nejo00@students.oamk.fi>
2023-11-13 14:42:43 +0200

    karuselliin margintop

--- src/components/Carouselli.js
+++ src/components/Carouselli.js
@@ -7,7 +7,7 @@ import { Image } from 'react-bootstrap';
 export const Carouselli = () => {
   return (
     
-    <Carousel className='text-center'>
+    <Carousel className='text-center' style={{marginTop:'20px'}}>
       <Carousel.Item>
         <Image src="https://www.io-tech.fi/wp-content/uploads/2022/10/rtx4090-00.jpg" alt='First slide' style={{ maxWidth: '100%', maxHeight: '100%', width: 'auto', height: 'auto', borderRadius:'5px' }} />
       
