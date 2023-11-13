054d4715b0e12c564acba035111fb2c755849c23
Aki <aki.stromberg@gmail.com>
2023-11-13 11:19:58 +0200

    footer icons and animations

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import './Header.css';
 import {  Head  } from "./Head";
 import React from 'react'
-import { Search } from './Search';
 import { Navi } from './Navi';
 
 export const Header = () => {
