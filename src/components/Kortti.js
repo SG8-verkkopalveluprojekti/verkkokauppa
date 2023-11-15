875d23e8b2884d87940c82c906f4f7af4a27de04
tyni99 <topias.tyni@hotmail.com>
2023-11-15 22:01:24 +0200

    fixed issue with footer

--- src/components/Kortti.js
+++ src/components/Kortti.js
@@ -6,7 +6,7 @@ import ListGroup from 'react-bootstrap/ListGroup';
 function Kortti() {
   return (
     <div className="d-flex justify-content-around  ">
-      <Card className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }} >
+      <Card className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px', marginBottom:'10rem' }} >
         <Card.Img  variant="top" src="https://www.io-tech.fi/wp-content/uploads/2022/10/rtx4090-00.jpg"  style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Näytönohjaimet</Card.Title>
@@ -17,7 +17,7 @@ function Kortti() {
         </Card.Body>
       </Card>
 
-      <Card className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
+      <Card className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px', marginBottom:'10rem' }}>
         <Card.Img variant="top" src="https://www.pctekreviews.com/Reviews/STRIX_B550E/01.jpg" style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Emolevyt</Card.Title>
@@ -28,7 +28,7 @@ function Kortti() {
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
-      <Card  className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
+      <Card  className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px', marginBottom:'10rem' }}>
         <Card.Img variant="top" src="https://hh2.pigugroup.eu/colours/241/450/8/2414508/memory-d4-4266-32gb-c17-gskill-tz-d9af0_reference.jpg" style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Muistit</Card.Title>
@@ -39,7 +39,7 @@ function Kortti() {
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
-      <Card className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
+      <Card className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px', marginBottom:'10rem' }}>
         <Card.Img variant="top" src="https://www.io-tech.fi/wp-content/uploads/2022/02/12400f-00.jpg" style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Prosessorit</Card.Title>
@@ -50,7 +50,7 @@ function Kortti() {
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
-      <Card className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
+      <Card className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px', marginBottom:'10rem' }}>
         <Card.Img variant="top" src="https://ic.jimms.fi/product/5/7/331652-ig800gg.jpg" style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Kotelot ja oheistuotteet</Card.Title>
@@ -61,7 +61,7 @@ function Kortti() {
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
-      <Card className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
+      <Card className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px', marginBottom:'10rem'}}>
         <Card.Img variant="top" src="https://ic.jimms.fi/product/4/0/251749-ig800gg.jpg" style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Virtalähteet</Card.Title>
