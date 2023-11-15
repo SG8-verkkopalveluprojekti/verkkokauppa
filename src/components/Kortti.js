8e27e0d9014f175bed1e3ac508b066ec5b1e0464
tyni99 <topias.tyni@hotmail.com>
2023-11-15 16:44:02 +0200

    fixed pictures

--- src/components/Kortti.js
+++ src/components/Kortti.js
@@ -6,7 +6,7 @@ function Kortti() {
   return (
     <div className="d-flex justify-content-around">
       <Card style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }} >
-        <Card.Img variant="top" src="https://www.io-tech.fi/wp-content/uploads/2022/10/rtx4090-00.jpg"  />
+        <Card.Img variant="top" src="https://www.io-tech.fi/wp-content/uploads/2022/10/rtx4090-00.jpg"  style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Näytönohjaimet</Card.Title>
           <Card.Text>
@@ -17,7 +17,7 @@ function Kortti() {
       </Card>
 
       <Card style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
-        <Card.Img variant="top" src="https://www.pctekreviews.com/Reviews/STRIX_B550E/01.jpg" />
+        <Card.Img variant="top" src="https://www.pctekreviews.com/Reviews/STRIX_B550E/01.jpg" style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Emolevyt</Card.Title>
           <Card.Text>
@@ -28,7 +28,7 @@ function Kortti() {
         </Card.Body>
       </Card>
       <Card style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
-        <Card.Img variant="top" src="https://hh2.pigugroup.eu/colours/241/450/8/2414508/memory-d4-4266-32gb-c17-gskill-tz-d9af0_reference.jpg" />
+        <Card.Img variant="top" src="https://hh2.pigugroup.eu/colours/241/450/8/2414508/memory-d4-4266-32gb-c17-gskill-tz-d9af0_reference.jpg" style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Muistit</Card.Title>
           <Card.Text>
@@ -39,7 +39,7 @@ function Kortti() {
         </Card.Body>
       </Card>
       <Card style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
-        <Card.Img variant="top" src="https://www.io-tech.fi/wp-content/uploads/2022/02/12400f-00.jpg" />
+        <Card.Img variant="top" src="https://www.io-tech.fi/wp-content/uploads/2022/02/12400f-00.jpg" style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Prosessorit</Card.Title>
           <Card.Text>
@@ -50,7 +50,7 @@ function Kortti() {
         </Card.Body>
       </Card>
       <Card style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
-        <Card.Img variant="top" src="https://ic.jimms.fi/product/5/7/331652-ig800gg.jpg" />
+        <Card.Img variant="top" src="https://ic.jimms.fi/product/5/7/331652-ig800gg.jpg" style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Kotelot ja oheistuotteet</Card.Title>
           <Card.Text>
@@ -61,7 +61,7 @@ function Kortti() {
         </Card.Body>
       </Card>
       <Card style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
-        <Card.Img variant="top" src="https://ic.jimms.fi/product/4/0/251749-ig800gg.jpg" />
+        <Card.Img variant="top" src="https://ic.jimms.fi/product/4/0/251749-ig800gg.jpg" style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Virtalähteet</Card.Title>
           <Card.Text>
