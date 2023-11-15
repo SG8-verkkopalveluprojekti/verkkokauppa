d83a3f9bd1b8782a62de77019aecfc3b170a80bc
Jonssoni <n2nejo00@students.oamk.fi>
2023-11-15 21:33:23 +0200

    kategoriat pienellä näytöllä listana

--- src/components/Kortti.js
+++ src/components/Kortti.js
@@ -1,12 +1,13 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function Kortti() {
   return (
-    <div className="d-flex justify-content-around">
-      <Card style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }} >
-        <Card.Img variant="top" src="https://www.io-tech.fi/wp-content/uploads/2022/10/rtx4090-00.jpg"  style={{height:'15rem'}}/>
+    <div className="d-flex justify-content-around  ">
+      <Card className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }} >
+        <Card.Img  variant="top" src="https://www.io-tech.fi/wp-content/uploads/2022/10/rtx4090-00.jpg"  style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Näytönohjaimet</Card.Title>
           <Card.Text>
@@ -16,7 +17,7 @@ function Kortti() {
         </Card.Body>
       </Card>
 
-      <Card style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
+      <Card className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
         <Card.Img variant="top" src="https://www.pctekreviews.com/Reviews/STRIX_B550E/01.jpg" style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Emolevyt</Card.Title>
@@ -27,7 +28,7 @@ function Kortti() {
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
-      <Card style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
+      <Card  className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
         <Card.Img variant="top" src="https://hh2.pigugroup.eu/colours/241/450/8/2414508/memory-d4-4266-32gb-c17-gskill-tz-d9af0_reference.jpg" style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Muistit</Card.Title>
@@ -38,7 +39,7 @@ function Kortti() {
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
-      <Card style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
+      <Card className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
         <Card.Img variant="top" src="https://www.io-tech.fi/wp-content/uploads/2022/02/12400f-00.jpg" style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Prosessorit</Card.Title>
@@ -49,7 +50,7 @@ function Kortti() {
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
-      <Card style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
+      <Card className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
         <Card.Img variant="top" src="https://ic.jimms.fi/product/5/7/331652-ig800gg.jpg" style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Kotelot ja oheistuotteet</Card.Title>
@@ -60,7 +61,7 @@ function Kortti() {
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
-      <Card style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
+      <Card className='d-none d-sm-block' style={{ width: '18rem', backgroundColor: 'grey', marginTop: '20px' }}>
         <Card.Img variant="top" src="https://ic.jimms.fi/product/4/0/251749-ig800gg.jpg" style={{height:'15rem'}}/>
         <Card.Body>
           <Card.Title>Virtalähteet</Card.Title>
@@ -71,9 +72,15 @@ function Kortti() {
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
-      
-      
+      <ListGroup className='d-block d-sm-none' style={{marginTop: '30px', width: '100%', padding:'30px' }}>
+      <ListGroup.Item>Näytönohjaimet</ListGroup.Item>
+      <ListGroup.Item>Emolevyt</ListGroup.Item>
+      <ListGroup.Item>Muistit</ListGroup.Item>
+      <ListGroup.Item>Prosessorit</ListGroup.Item>
+      <ListGroup.Item>Kotelot ja oheistuotteet</ListGroup.Item>
+    </ListGroup>
     </div>
+    
   );
 }
 
