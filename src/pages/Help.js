2f43b8af2443f685ed3a8706f9de5e360b42ae6c
Aki <aki.stromberg@gmail.com>
2023-11-16 13:43:41 +0200

    updates

--- src/pages/Help.js
+++ src/pages/Help.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export const Help = () => {
   return (
-    <div>Help</div>
-  )
-}
+    <div className='container'>
+      <div className="grid">
+        <div className="grid_col-12">
+          <h1>Apua vaivaan kuin vaivaan</h1>
+            <p>kekeke</p>
+        </div>
+      </div>
+    </div>
+  );
+};
