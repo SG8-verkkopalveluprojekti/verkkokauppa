d073acb302875be29a3eb2f1ed21dae77f4e5c48
Elias K <127874391+n2koel00@users.noreply.github.com>
2023-11-22 21:28:15 +0200

    Cpu kuva test

--- src/pages/Cpu.js
+++ src/pages/Cpu.js
@@ -2,6 +2,7 @@ import React from 'react'
 import axios from 'axios'
 import { useState,useEffect } from 'react';
 import { Card, Button} from 'react-bootstrap'
+import Corsair from "./Kuvat/Corsair 32GB.jpg"
 
 export const  Cpu = () => {
 
@@ -17,7 +18,7 @@ export const  Cpu = () => {
       <div className="row">
         {products && products.map(product => (
           <Card key={product.id} className='col-md-3 mb-3 d-none d-sm-block' style={{ width: '250px', backgroundColor: 'grey', marginTop: '60px', marginBottom:'10rem', marginLeft:'10px'}}>
-            <Card.Img variant="top" src={product.imageUrl || 'https://www.io-tech.fi/wp-content/uploads/2022/02/12400f-00.jpg'} style={{height:'15rem', marginTop:'5px', padding:'15px'}}/>
+            <Card.Img src={Corsair} style={{height:'15rem', marginTop:'5px', padding:'15px'}}/>
             <Card.Body>
               <Card.Title>{product.productName}</Card.Title>
               <Card.Text>
