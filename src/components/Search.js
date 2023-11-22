ac3ca6a9713dfe1d5aa1740ebc5d41fe12c3df0a
Aki <aki.stromberg@gmail.com>
2023-11-22 17:58:59 +0200

    formatting

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,22 +4,21 @@ import "./Search.css";
 import axios from "axios";
 import { useState } from "react";
 
-
 export const Search = () => {
-
   const [searchTerm, setSearchTerm] = useState("");
   const [products, setProducts] = useState([]);
 
   const handleSearchClick = async () => {
     try {
-      const response = await axios.get(`http://localhost:3001/products?search=${searchTerm}`)
-      setProducts(response.data)
+      const response = await axios.get(
+        `http://localhost:3001/products?search=${searchTerm}`
+      );
+      setProducts(response.data);
       console.log(response);
     } catch (error) {
-      console.error("Error fetching products", error)
-      
+      console.error("Error fetching products", error);
     }
-  }
+  };
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
@@ -31,19 +30,23 @@ export const Search = () => {
     <>
       <section className="search">
         <div className="container c_flex">
-          <div className="search-box f_flex">         
-            <input  type="text"
-                    placeholder="Etsi tuotteita"
-                    value={searchTerm}
-                    onKeyDown={handleKeyDown}
-                    onChange={(e) => setSearchTerm(e.target.value)}>
-            </input>            
-            <button className="searchButton" type="button" onClick={handleSearchClick}>
-              
+          <div className="search-box f_flex">
+            <input
+              type="text"
+              placeholder="Etsi tuotteita"
+              value={searchTerm}
+              onKeyDown={handleKeyDown}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            ></input>
+            <button
+              className="searchButton"
+              type="button"
+              onClick={handleSearchClick}
+            >
               <i className="fa fa-search"></i>
             </button>
           </div>
-          
+
           <div className="cart-icon f_flex width">
             <div className="cart">
               <Link to="/cart">
@@ -63,7 +66,6 @@ export const Search = () => {
           </div>
         ))}
       </section>
-      
     </>
   );
 };
