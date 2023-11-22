719487ee11877b2e7f3d4fe51162838c1b251c71
Aki <aki.stromberg@gmail.com>
2023-11-22 17:55:41 +0200

    formating

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-import { Link } from "react-router-dom";
 import { useState } from 'react';
 import axios from 'axios';
 
