05542881d69db8179628d0dab68a3b4bb8c80e23
tyni99 <topias.tyni@hotmail.com>
2023-12-13 10:48:47 +0200

    käyttäjälistan muokkailua

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, ListGroup, ListGroupItem } from 'react-bootstrap';
 import axios from 'axios';
 
 export const Users = () => {
@@ -14,15 +14,18 @@ export const Users = () => {
   }, []); 
 
   return (
-    <div className='text-center' style={{border:"2px solid grey", margin:"10px", borderRadius:"10px"}}>
+    <div className='text-center' style={{margin:"10px", borderRadius:"10px"}}>
       <h2 style={{marginTop:"10px", marginBottom:"10px"}}>Käyttäjälista</h2><hr></hr>
-      <ul>
+      <ListGroup numbered>
         {users.map(user => (
-          <li key={user.username}>
-            Etunimi: {user.first_name}<br></br>Sukunimi: {user.last_name}<br></br> Käyttäjänimi:  {user.username} <br></br><hr></hr>
-          </li>
+          <ListGroupItem style={{backgroundColor: "#dbdada"}} key={user.username}> <br></br>Etunimi: {user.first_name} <br></br>   Sukunimi: {user.last_name} <br></br>    Käyttäjänimi:  {user.username}<br></br>
+          {/** Nämä buttonit ei toiminnassa*/}
+          <Button variant='dark' size='sm' style={{marginRight:"5px"}}>Muokkaa käyttäjän tietoja</Button>
+          <Button variant='danger' size='sm'>Poista käyttäjä</Button>
+          </ListGroupItem>
+          
         ))}
-      </ul>
+      </ListGroup>
       <Button variant='dark' size='sm' href="/Admin" style={{margin:"10px",marginBottom:"20px"}}>Takaisin tuotteiden lisäykseen</Button>
     </div>
   );
